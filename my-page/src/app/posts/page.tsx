dcba63146c2e0c622b8e702d95046303dfa07976
scottyjoppy <alexanderdelisle@yahoo.ca>
2025-03-27 13:55:42 +0100

    first commit

--- my-page/src/app/posts/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Blog() {
-    return <h1>Posts!</h1>
-}
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: '/my-page', // Adjust this to the folder name if it's different
 };
 
 export default nextConfig;
