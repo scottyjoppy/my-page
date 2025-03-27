8a55b2438e97c694432a3a540c49b49652f8d8cf
scottyjoppy <alexanderdelisle@yahoo.ca>
2025-03-27 13:23:06 +0100

    .

--- my-page/next.config.ts
+++ my-page/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/my-page', // Adjust this to the folder name if it's different
 };
 
 export default nextConfig;
