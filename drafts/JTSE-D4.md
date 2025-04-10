de2e4d066118d8c6ab0a50a7fd92872e48942666
scottyjoppy <alexanderdelisle@yahoo.ca>
2025-04-10 13:22:11 +0200

    Added vercel analytics

---
+++ drafts/JTSE-D4.md
@@ -0,0 +1,13 @@
+---
+title: "Day 4: SanityIO & Figma"
+blogSeries: "Journey To: Software Engineer"
+blogLine: "From warehouse racks to full stack"
+date: "April 10, 2025"
+description: "Headless Content Management Systems and Web Page design."
+---
+
+Hello,
+
+<br>
+
+today I was introduced to the 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Layout from "@/components/Layout";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SpeedInsights />
         <Layout>{children}</Layout>
       </body>
     </html>
