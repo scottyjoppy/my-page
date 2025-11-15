21a79a18a17726e6009f065085511c20da3c075a
scottyjoppy <alexanderdelisle@yahoo.ca>
2025-11-15 12:09:29 +0100

    Style: added image to homepage and changed main font

--- src/components/new-styles/pages/home/HomePage.tsx
+++ src/components/new-styles/pages/home/HomePage.tsx
@@ -1,9 +1,28 @@
-import styles from "./HomePage.module.css"
+import Link from "next/link";
+import styles from "./HomePage.module.css";
 
 const HomePage = () => {
   return (
     <>
-      <section className={styles.container}>Something</section>
+      <section className={styles.container}>
+        <div className={styles["welcome-container"]}>
+          <div className={styles["welcome-text"]}>
+            <h2 className="uppercase">Welcome</h2>
+            <p>My name is Alexander Delisle</p>
+            <p>I'm here to learn</p>
+          </div>
+          <div className={styles["image-container"]}>
+            <Link href="/about">
+              <img
+                src="/images/alex-image.jpg"
+                alt="a picture of alex"
+                className={styles.image}
+              />
+            </Link>
+          </div>
+        </div>
+        <div className={styles["info-container"]}></div>
+      </section>
     </>
   );
 };
