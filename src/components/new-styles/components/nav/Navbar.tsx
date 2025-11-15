6514b3983881d2c11a2d79674a09a42c458811f3
scottyjoppy <alexanderdelisle@yahoo.ca>
2025-11-15 12:09:29 +0100

    Style: added image to homepage and changed main font

--- src/components/new-styles/components/nav/Navbar.tsx
+++ src/components/new-styles/components/nav/Navbar.tsx
@@ -1,27 +1,33 @@
+import { menuItems } from "@/components/navbar/menuItems";
 import Link from "next/link";
 import styles from "./Navbar.module.css";
-import { MenuItem } from "@/components/navbar/types";
-
-const items = 
 
 async function Navbar() {
   return (
     <>
       <nav className={styles.nav}>
         <Link href="/" className={styles.icon}>
-          ALX
+          <span className={styles["icon-text"]}>ALEX</span>
         </Link>
         <div className={styles["pages-container"]}>
-          {MenuItem.map}
-          <Link href="/" className={styles["page-link"]}>
-            something
-          </Link>
-          <Link href="/" className={styles["page-link"]}>
-            something
-          </Link>
-          <Link href="/" className={styles["page-link"]}>
-            something
-          </Link>
+          <div className={styles["mobile-nav-container"]}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              className={styles["svg-nav"]}
+            >
+              <g id="_icons">
+                <path d="M5 7h14c.6 0 1-.4 1-1s-.4-1-1-1H5c-.6 0-1 .4-1 1s.4 1 1 1zM5 13h14c.6 0 1-.4 1-1s-.4-1-1-1H5c-.6 0-1 .4-1 1s.4 1 1 1zM5 19h14c.6 0 1-.4 1-1s-.4-1-1-1H5c-.6 0-1 .4-1 1s.4 1 1 1z" />
+              </g>
+            </svg>
+          </div>
+          {menuItems.map((item, key) => {
+            return (
+              <Link href={item.href} key={key} className={styles["page-link"]}>
+                <h3>{item.label}</h3>
+              </Link>
+            );
+          })}
         </div>
       </nav>
     </>
