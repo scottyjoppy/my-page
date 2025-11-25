14a7f0baf0ca2827df9e380640e860409944b63c
scottyjoppy <alexanderdelisle@yahoo.ca>
2025-11-25 12:52:16 +0100

    Chore: prep backend for DO

---
+++ backend/config/env/production/database.ts
@@ -0,0 +1,22 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      connectionString: env("DATABASE_URL"),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "strapi"),
+      password: env("DATABASE_PASSWORD", "strapi"),
+      schema: env("DATABASE_SCHEMA", "public"), // Not required
+      ssl: env.bool("DATABASE_SSL", false) && {
+        ca: env("DATABASE_CA"),
+      },
+    },
+    pool: {
+      min: env.int("DATABASE_POOL_MIN", 2),
+      max: env.int("DATABASE_POOL_MAX", 10),
+    },
+    debug: false,
+  },
+});
