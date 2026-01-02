d25175dc69aa7c4c8b1667c03cd5c61543249ba4
scottyjoppy <alexanderdelisle@yahoo.ca>
2026-01-02 19:57:59 +0100

    Feat: testing out how to pull from supabase buckets

---
+++ src/composables/useBuckets.ts
@@ -0,0 +1,42 @@
+import { createClient } from "@/lib/supabase/client";
+import { useEffect, useState } from "react";
+
+const supabase = createClient();
+
+export const useAboutBucket = () => {
+  const [files, setFiles] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchFiles = async () => {
+      try {
+        setLoading(true);
+
+        const { data, error } = await supabase.storage
+          .from("about_page")
+          .list("public");
+
+        if (error) throw error;
+
+        const filesWithUrls =
+          data?.map((file) => ({
+            ...file,
+            url: supabase.storage
+              .from("about_page")
+              .getPublicUrl(`public/${file.name}`).data.publicUrl,
+          })) ?? [];
+
+        setFiles(filesWithUrls);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchFiles();
+  }, []);
+
+  return { files, loading, error };
+};
