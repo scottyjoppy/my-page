6003f63a8bb678036261f89e83211703baaf698c
scottyjoppy <alexanderdelisle@yahoo.ca>
2026-01-04 21:56:49 +0100

    Refactor: changed colors

--- src/components/navbar/types.ts
+++
@@ -1,8 +0,0 @@
-import { IconsMap } from "./iconMap";
-
-export type MenuItem = {
-  label: string;
-  iconName?: keyof typeof IconsMap; // Icon name as a key of IconsMap
-  href: string; // Link for the menu item
-  dropdown?: { label: string; href: string }[];
-};
