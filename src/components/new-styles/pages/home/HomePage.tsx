d4f79935a672d214aeeaf79432f549b88dd6029f
scottyjoppy <alexanderdelisle@yahoo.ca>
2026-01-06 21:24:58 +0100

    Style: ascii stylings on the home page

--- src/components/new-styles/pages/home/HomePage.tsx
+++ src/components/new-styles/pages/home/HomePage.tsx
@@ -1,28 +1,45 @@
 "use client";
 
 import { useAboutBucket } from "@/composables/useBuckets";
+import { useRef } from "react";
 import ImageBorder from "../../components/ascii/ImageBorder";
+import RomanCol from "../../components/ascii/RomanCol";
 import styles from "./HomePage.module.css";
 
 const HomePage = () => {
   const { files } = useAboutBucket();
 
+  const sectionRef = useRef<HTMLElement | null>(null);
+
   return (
     <>
-      <section className={styles.container}>
+      <section ref={sectionRef} className={styles.container}>
         <div className={styles["welcome-container"]}>
+          <h2 className={styles.title}>Welcome</h2>
           <div className={styles["welcome-text"]}>
-            <h2 className="uppercase">Welcome</h2>
-            <p>My name is Alexander Delisle</p>
-            <p>I'm here to learn</p>
+            <p className={`${styles["small-p"]} text-left`}>
+              You've reached the personal page of this guy -----{">"}
+            </p>
+            {files[1] && (
+              <div className={styles["image-container"]}>
+                <img src={files[1].url} className={styles.image} />
+                <ImageBorder className={styles["ascii-img-pos"]} />
+              </div>
+            )}
+            <p className={`${styles["small-p"]} text-right`}>
+              My own digital place to share blogs, projects, and{" "}
+              <em>more...</em>
+            </p>
           </div>
-          {files[1] && (
-            <div className={styles["image-container"]}>
-              <img src={files[1].url} className={styles.image} />
-              <ImageBorder className={styles["ascii-pos"]} />
-            </div>
-          )}
         </div>
+        <RomanCol
+          className={styles["ascii-col-pos-l"]}
+          parentRef={sectionRef}
+        />
+        <RomanCol
+          className={styles["ascii-col-pos-r"]}
+          parentRef={sectionRef}
+        />
       </section>
     </>
   );
