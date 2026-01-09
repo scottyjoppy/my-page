2ad25bb899c13cc51fb8bbbfecd2666f1ffa6657
scottyjoppy <alexanderdelisle@yahoo.ca>
2026-01-09 08:51:41 +0100

    Style: adding the tree

--- src/components/new-styles/pages/timeline/TimelineSection.tsx
+++ src/components/new-styles/pages/timeline/TimelineSection.tsx
@@ -2,14 +2,20 @@
 
 import CreationOfAdam from "../../components/ascii/CreationOfAdam";
 import DirectionalBorder from "../../components/ascii/DirectionalBorder";
+import TreeImage from "../../components/ascii/TreeImage";
 import styles from "./TimelineSection.module.css";
 
 const TimelineSection = () => {
   return (
     <>
       <section className={styles.container}>
-        <DirectionalBorder className={styles["ascii-border"]}/>
         <CreationOfAdam className={styles["ascii-art"]} />
+        <div className={styles["tree-container"]}>
+          <p className={styles["grid-box"]}>Thing 1</p>
+          <p className={styles["grid-box"]}>Thing 2</p>
+          <p className={styles["grid-box"]}>Thing 3</p>
+          <TreeImage className={styles["ascii-tree"]} />
+        </div>
       </section>
     </>
   );
